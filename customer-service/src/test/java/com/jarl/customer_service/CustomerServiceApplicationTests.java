621f9cceeb18790bc20ff967787d1bd08c437ba2
Alejandro Rivas <jarlalejor@gmail.com>
2025-05-17 20:06:23 -0500

    creacion de proyectos bases

---
+++ customer-service/src/test/java/com/jarl/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarl.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
